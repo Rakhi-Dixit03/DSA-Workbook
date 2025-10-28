f81188bcd8c103c2b6f1cc48394b559a195e0628
RakhiDixit <rjbr1508@gmail.com>
2025-10-28 16:41:35 +0530

    Added Binary Tree Traversals

---
+++ src/Binary_Tree/Tree_Traversals.java
@@ -0,0 +1,54 @@
+package Binary_Tree;
+
+public class Tree_Traversals {
+
+
+    public static void main(String[] args) {
+
+        TreeNode root=new TreeNode(1,new TreeNode(2,new TreeNode(4),
+                new TreeNode(5)),new TreeNode(3,new TreeNode(6),null));
+
+        preOrder(root);
+        System.out.println();
+        inOrder(root);
+        System.out.println();
+        postOrder(root);
+
+
+    }
+    private static void  preOrder(TreeNode root ){
+
+         //Base Case
+        if(root==null)return;
+
+        System.out.print(root.val+" ");
+        preOrder(root.left);
+        preOrder(root.right);
+
+    }
+
+    private static void inOrder(TreeNode root){
+
+        //Base Case
+        if(root==null)return;
+
+        inOrder(root.left);
+        System.out.print(root.val+" ");
+        inOrder(root.right);
+
+    }
+
+    private static void postOrder(TreeNode root){
+
+        //Base Case
+
+        if(root==null)return;
+
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.val+" ");
+
+    }
+
+
+}
