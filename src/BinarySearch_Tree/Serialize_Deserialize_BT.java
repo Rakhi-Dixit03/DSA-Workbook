18f9448a955713e0943f3d789965af37d92e0821
RakhiDixit <rjbr1508@gmail.com>
2025-11-20 19:42:28 +0530

    Added Serialize & DeSerialize (BFS) Approach

--- src/BinarySearch_Tree/Serialize_Deserialize_BT.java
+++ src/BinarySearch_Tree/Serialize_Deserialize_BT.java
@@ -21,7 +21,7 @@ public class Serialize_Deserialize_BT {
 
         Serialize_Deserialize_BT deser=new Serialize_Deserialize_BT();
 
-        TreeNode ans = deser.deserialize(ser.serialize(root));
+        TreeNode ans = deser.deserializeLevelOrder(ser.serializeLevelOrder(root));
         System.out.println("After Serialisation : ");
         Tree_Traversals.preOrder(ans);
 
@@ -85,8 +85,76 @@ public class Serialize_Deserialize_BT {
     }
 
 
+   //using Level Order Traversal
+
+    public String serializeLevelOrder(TreeNode root){
 
 
+        if(root==null)return "";
+        StringBuilder sb=new StringBuilder();
+        Queue<TreeNode>q=new LinkedList<>();
 
+        q.add(root);
+
+        while(!q.isEmpty()){
+
+            TreeNode node=q.poll();
+
+            if(node==null){
+
+                sb.append("null,");
+                continue;
+            }
+
+            sb.append(node.val).append(",");
+            q.add(node.left);
+            q.add(node.right);
+
+        }
+
+      return sb.toString();
+    }
+
+    //Using Level Order Traversal
+
+    public TreeNode deserializeLevelOrder(String data){
+
+        String[]  values=data.split(",");
+
+        int idx=0;
+
+        if(values[idx].equals("null"))return null;
+
+        Queue<TreeNode>q=new LinkedList<>();
+
+        TreeNode root=new TreeNode(Integer.parseInt(values[idx++]));
+        q.add(root);
+
+        while(!q.isEmpty()){
+
+
+            TreeNode node=q.poll();
+
+            if(!values[idx].equals("null")){
+
+                TreeNode left=new TreeNode(Integer.parseInt(values[idx]));
+                node.left=left;
+                q.add(left);
+            }
+             idx++;
+
+            if(idx<values.length && !values[idx].equals("null")){
+
+                TreeNode right=new TreeNode(Integer.parseInt(values[idx++]));
+
+                node.right=right;
+                q.add(right);
+            }
+
+
+        }
+
+        return root;
+    }
 
 }
