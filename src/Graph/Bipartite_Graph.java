08f87db699b2a6cd81c92ef827570d09a871153e
RakhiDixit <rjbr1508@gmail.com>
2025-12-29 18:40:37 +0530

    Added 2nd Method to find Bipartite Graph

--- src/Graph/Bipartite_Graph.java
+++ src/Graph/Bipartite_Graph.java
@@ -1,6 +1,7 @@
 package Graph;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -19,7 +20,19 @@ public class Bipartite_Graph {
         graph.buildGraph(edges);
 
         System.out.println("Is the given graph Bipartite or Not ? : "+isBipartite(graph.graph));
+        boolean ans=false;
 
+        if(isCycle(graph.graph)){
+            if(cycleNodes % 2 == 0){//if cycle exists cycle node count should be even
+                ans=true;
+            }
+
+        }else{
+            //if cycle doesn't exist then by default it is a bipartite
+            ans=true;
+        }
+
+        System.out.println("Is the given graph Bipartite or Not using 2nd method ? : "+ans);
 
     }
 
@@ -66,6 +79,66 @@ public class Bipartite_Graph {
 
         }
         return true;
+    }
+
+    static int cycleNodes;
+    public static boolean isCycle(ArrayList<Edge>[] graph){
+
+        int v=graph.length;
+        int[] parent =new int[v];
+        Arrays.fill(parent,-1);
+        boolean[] vis = new boolean[v];
+
+        for(int i=0;i<v;i++){
+
+            if(!vis[i]){
+
+               if(dfs(i,graph,vis,parent,-1)){
+                 return true;
+               }
+
+            }
+        }
+
+        return false;
+
+    }
+
+    //Through Cycle detection logic ,if a graph contains a cycle then we have to calculate the number of nodes in the cycle
+
+    public static boolean dfs(int curr,ArrayList<Edge>[] graph,boolean[] vis,int[] parent,int par){
+
+        vis[curr]=true;
+        parent[curr] = par;
+
+        for(Edge e : graph[curr]){
+
+            if(!vis[e.des]){
+                if(dfs(e.des,graph,vis,parent,curr)){
+                    return true;
+                }
+
+            }else if(vis[e.des] && par!=e.des){//back edge
+
+                countCycleNodes(curr,e.des,parent);
+                return true;
+
+            }
+        }
+
+    return false;
+    }
+
+    public static void countCycleNodes(int st,int end,int[] parent){
+       cycleNodes=1;
+        int curr=st;
+
+        while(curr!=end){
+
+            curr=parent[curr];
+            cycleNodes++;
+
+        }
     }
 
 }
