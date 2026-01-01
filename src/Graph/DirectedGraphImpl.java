dc8c39c5393d5cc1db79bd0eddbd284bfaa5a172
RakhiDixit <rjbr1508@gmail.com>
2026-01-01 13:13:13 +0530

    Added Directed Graph Implementation Questions

---
+++ src/Graph/DirectedGraphImpl.java
@@ -0,0 +1,80 @@
+package Graph;
+
+import java.util.ArrayList;
+
+public class DirectedGraphImpl {
+
+    static class Edge {
+
+        int src;
+        int des;
+        int wt;
+
+        public Edge(int src, int des, int wt) {
+            this.src = src;
+            this.des = des;
+            this.wt = wt;
+        }
+
+    }
+
+    ArrayList<Edge>[] graph;
+
+    public DirectedGraphImpl(int v) {
+
+        graph = new ArrayList[v];
+
+        for (int i = 0; i < graph.length; i++) {
+
+            graph[i] = new ArrayList<>();//initialising with empty Arraylists
+        }
+
+
+    }
+
+    public void buildGraph(int[][] edgeInfo) {
+
+        for (int[] edge : edgeInfo) {
+
+            int u = edge[0];
+            int v = edge[1];
+
+            graph[u].add(new Edge(u, v, 1));
+
+        }
+
+    }
+
+    public void printGraph() {
+
+        for (int i = 0; i < graph.length; i++) {
+
+            System.out.print("Neighbours of vertex " + i + " : { ");
+
+            for (Edge e : graph[i]) {
+
+                System.out.print(e.des + " ");
+
+
+            }
+
+            System.out.println("}");
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+
+        DirectedGraphImpl graph1 = new DirectedGraphImpl(5);//passing number of vertices
+
+        int[][] edges = {{0, 1}, {1, 2}, {1, 3}, {3, 2}, {2, 4}};//directed edges
+
+        graph1.buildGraph(edges);
+        graph1.printGraph();
+
+
+    }
+
+}
+
