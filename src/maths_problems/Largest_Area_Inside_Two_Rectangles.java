b8f188ad6b26f80405fb008d55228dad25ec2048
RakhiDixit <rjbr1508@gmail.com>
2026-01-17 22:44:20 +0530

    Added Array+Geometry Question

---
+++ src/maths_problems/Largest_Area_Inside_Two_Rectangles.java
@@ -0,0 +1,37 @@
+package maths_problems;
+
+import java.sql.SQLOutput;
+
+public class Largest_Area_Inside_Two_Rectangles {
+
+    public static void main(String[] args) {
+
+        int[][] bottomLeft={{1,1},{1,3},{1,5}};
+        int[][] topRight={{5,5},{5,7},{5,9}};
+
+        System.out.println("Largest Area of Square Inside Two Rectangles : "+largestSquareArea(bottomLeft,topRight));
+
+    }
+
+    public static long largestSquareArea(int[][] bottomLeft, int[][] topRight) {
+        int n = bottomLeft.length;
+        long maxSide = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int w =
+                        Math.min(topRight[i][0], topRight[j][0]) -
+                                Math.max(bottomLeft[i][0], bottomLeft[j][0]);
+                int h =
+                        Math.min(topRight[i][1], topRight[j][1]) -
+                                Math.max(bottomLeft[i][1], bottomLeft[j][1]);
+                int side = Math.min(w, h);
+
+                maxSide = Math.max(maxSide, side);
+            }
+        }
+
+        return maxSide * maxSide;
+    }
+}
+
