0d1106843e411266b53f4660b2ba8310dc2e4793
RakhiDixit <rjbr1508@gmail.com>
2026-01-22 17:29:56 +0530

    Modified Approach

--- src/Bit_Manipulation/Construct_Mini_Bitwise_Arr2.java
+++ src/Bit_Manipulation/Construct_Mini_Bitwise_Arr2.java
@@ -15,19 +15,45 @@ public class Construct_Mini_Bitwise_Arr2 {
 
        }
 
-        public static int[] minBitwiseArray(List<Integer> nums) {
-            int n = nums.size();
-            int[] result = new int[n];
-            for (int i = 0; i < n; i++) {
-                int x = nums.get(i);
-                int res = -1;
-                int d = 1;
-                while ((x & d) != 0) {
-                    res = x - d;
-                    d <<= 1;
-                }
-                result[i] = res;
+    public static int[] minBitwiseArray(List<Integer> nums) {
+
+        int n = nums.size();
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+
+            int num=nums.get(i);
+
+            if(num==2){
+
+                result[i]=-1;
+                continue;
+
             }
-            return result;
+
+            int ans;
+            //we are going till 31 because this much of bits are sufficient to represent a number up-to 10^9
+            for(int j=1;j<32;j++){
+
+                if((num&(1<<j))>0){
+
+                    continue;
+
+                }
+
+                //Replace with 0
+
+                ans=num^(1<<(j-1));
+                result[i]=ans;
+                break;
+
+            }
+
+
         }
+        return result;
+    }
+
+
+
 }
