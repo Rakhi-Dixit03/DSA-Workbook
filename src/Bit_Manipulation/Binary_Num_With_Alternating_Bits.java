96bc5a72c15c2309b9f793c87742d399a894d831
RakhiDixit <rjbr1508@gmail.com>
2026-02-18 22:19:42 +0530

    Added Bits Question

---
+++ src/Bit_Manipulation/Binary_Num_With_Alternating_Bits.java
@@ -0,0 +1,68 @@
+package Bit_Manipulation;
+
+public class Binary_Num_With_Alternating_Bits {
+
+    public static void main(String[] args) {
+
+        Binary_Num_With_Alternating_Bits obj=new Binary_Num_With_Alternating_Bits();
+
+        System.out.println("Does N have Alternating Bits : "+obj.hasAlternatingBits1(5));
+        System.out.println("Does N have Alternating Bits : "+obj.hasAlternatingBits1(8));
+        System.out.println("Does N have Alternating Bits : "+obj.hasAlternatingBits1(21));
+    }
+    public boolean hasAlternatingBits1(int n) {
+
+        String binStr=Integer.toBinaryString(n);
+
+        for(int i=1;i<binStr.length();i++){
+
+            if(binStr.charAt(i)==binStr.charAt(i-1)){//Comparing bits as characters
+                return false;
+            }
+
+        }
+        return true;
+
+    }
+
+
+    public boolean hasAlternatingBits2(int n) {
+        if(n==1){
+            return true;
+        }
+        int currBit=n&1;
+        int lastBit=currBit;
+        n>>=1;
+        while(n>0){
+
+            currBit=n&1;
+            if(lastBit==currBit){
+                return false;
+            }
+            lastBit=currBit;
+            n>>=1;
+
+        }
+        return true;
+
+    }
+
+    public boolean hasAlternatingBits3(int n) {
+        if(n==1){
+            return true;
+        }
+
+        String binStr=Integer.toBinaryString(n);
+
+        for(int i=0;i<binStr.length()-1;i++){
+
+            if(((int)binStr.charAt(i)^(int)binStr.charAt(i+1))==0){//using XOR Operator
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+
+}
