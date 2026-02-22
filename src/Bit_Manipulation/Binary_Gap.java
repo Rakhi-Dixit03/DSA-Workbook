59842d690fea2f5d8734ca2e9f33d9761dc3c37d
RakhiDixit <rjbr1508@gmail.com>
2026-02-22 11:02:32 +0530

    Added Bits Question

---
+++ src/Bit_Manipulation/Binary_Gap.java
@@ -0,0 +1,58 @@
+package Bit_Manipulation;
+
+public class Binary_Gap {
+
+    public static void main(String[] args) {
+        System.out.println("Maximum Binary Gap :  "+binaryGap(10));
+    }
+    //TC-O(N)
+    public static int binaryGap(int n) {
+
+        String binStr = Integer.toBinaryString(n);
+
+        //30 bits are needed to rep. 10^9 integer  value
+        int binGap = 0;
+        int lastIdx = -1;
+        for (int i = binStr.length() - 1; i >= 0; i--) {
+
+            if (binStr.charAt(i) == '1') {
+
+                if (lastIdx == -1) {
+                    lastIdx = i;
+                } else {
+
+                    int gap = Math.abs(i - lastIdx);
+                    binGap = Math.max(binGap, gap);
+                    lastIdx = i;
+
+                }
+
+            }
+        }
+        return binGap;
+    }
+
+    //TC-O(1)
+    public static int binaryGap1(int n) {
+
+
+        //30 bits are needed to rep. 10^9 integer  value
+        int binGap = 0;
+        int lastIdx = -1;
+        for (int i = 0; i <31 ; i++) {
+
+            if(((n>>i)&1)==1){//extracting digit at index i from [least significant to most significant]
+
+                if(lastIdx==-1)lastIdx=i;
+                int gap = Math.abs(i - lastIdx);
+                binGap = Math.max(binGap, gap);
+                lastIdx = i;
+
+
+            }
+        }
+        return binGap;
+    }
+
+}
+
