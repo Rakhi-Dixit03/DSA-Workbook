b3dfe7ba055b568cfb28ecc91e66089f0d7f490e
RakhiDixit <rjbr1508@gmail.com>
2026-05-14 19:36:47 +0530

    Added Array Question

---
+++ src/Arrays/Is_Array_Good.java
@@ -0,0 +1,55 @@
+package Arrays;
+
+public class Is_Array_Good {
+
+    //Validating array properties by checking length, maximum value,
+    // and element frequency constraints.
+    //TC-O(N)
+    //SC-O(N)
+    public static boolean isGood(int[] numbers) {
+
+        int n = numbers.length;
+        int max = Integer.MIN_VALUE;
+
+        boolean []exist = new boolean[n + 1];
+
+        for (int j : numbers) {
+
+            max = Math.max(max, j);
+
+        }
+
+        if (n != (max + 1))
+            return false;
+        int countMax = 0;
+
+        for (int num : numbers) {
+
+            if (num > max) {
+                return false;
+
+            } else if (num == max) {
+                countMax++;
+
+            } else {
+                if (exist[num]) {
+                    return false;
+                } else {
+                    exist[num] = true;
+                }
+
+            }
+
+        }
+
+        return countMax == 2;
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Is Array Good ? : "+isGood(new int[]{1,3,4,2,4}));
+    }
+
+
+}
