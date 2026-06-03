cd6407773c5ca9d67481d4db852c03ccdbd30d0f
RakhiDixit <rjbr1508@gmail.com>
2026-06-03 19:01:37 +0530

    Added Greedy Question

---
+++ src/Greedy/Earliest_Finish_Time_For_Land_Water_Ride_2.java
@@ -0,0 +1,27 @@
+package Greedy;
+//TC-O(M+N)
+//Minimizing total time by independently finding the earliest finish time for each ride category and combining them optimally.
+
+public class Earliest_Finish_Time_For_Land_Water_Ride_2 {
+
+    final static int MAX = 300005;
+
+    public int earliestFinishTime(int[] la, int[] lb, int[] wa, int[] wb) {
+        int l = MAX, w = MAX, minL = MAX, minW = MAX;
+        int n = la.length, m = wa.length;
+
+        for (int i = 0; i < n; ++i)
+            l = Math.min(l, la[i] + lb[i]);
+
+        for (int i = 0; i < m; ++i) {
+            w = Math.min(w, wa[i] + wb[i]);
+            minL = Math.min(minL, Math.max(wa[i], l) + wb[i]);
+        }
+
+        for (int i = 0; i < n; ++i)
+            minW = Math.min(minW, Math.max(la[i], w) + lb[i]);
+
+        return Math.min(minW, minL);
+    }
+}
+
