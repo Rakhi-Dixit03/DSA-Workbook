80817a8948ce2d2298c79e67add31418aeca425a
RakhiDixit <rjbr1508@gmail.com>
2026-06-06 22:12:52 +0530

    Added prefix-sum Question

---
+++ src/Prefix_Sum/Left_And_Right_Sum.java
@@ -0,0 +1,23 @@
+package Prefix_Sum;
+
+public class Left_And_Right_Sum {
+
+    public int[] leftRightDifference(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        int leftSum = 0;
+        for (int i = 0; i < n; ++i) {
+            ans[i] = leftSum;
+            leftSum += nums[i];
+        }
+
+        int rightSum = 0;
+        for (int i = n - 1; i >= 0; --i) {
+            ans[i] = Math.abs(ans[i] - rightSum);
+            rightSum += nums[i];
+        }
+
+        return ans;
+    }
+}
