ab00205e98628abf01574075357425474882aca8
RakhiDixit <rjbr1508@gmail.com>
2026-06-08 16:51:34 +0530

    Added Array  Question

---
+++ src/Arrays/Partition_Array_By_Pivot.java
@@ -0,0 +1,32 @@
+package Arrays;
+
+import java.util.*;
+
+public class Partition_Array_By_Pivot {
+
+    public int[] pivotArray(int[] nums, int pivot) {
+        LinkedList<Integer> less = new LinkedList<>();
+        LinkedList<Integer> equal = new LinkedList<>();
+        LinkedList<Integer> greater = new LinkedList<>();
+        for (int num : nums) {
+            if (num < pivot) {
+                less.add(num);
+            } else if (num > pivot) {
+                greater.add(num);
+            } else {
+                equal.add(num);
+            }
+        }
+        less.addAll(equal);
+        less.addAll(greater);
+
+        int i = 0;
+        int[] ans = new int[nums.length];
+        for (int num : less) {
+            ans[i++] = num;
+        }
+        return ans;
+    }
+}
+
+
