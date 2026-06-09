08808af378625523757c054044b0016ae13169c4
RakhiDixit <rjbr1508@gmail.com>
2026-06-09 22:19:14 +0530

    Added Array Question

---
+++ src/Arrays/Max_Total_SubArr_Val.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+public class Max_Total_SubArr_Val {
+
+    public long maxTotalValue(int[] A, int k) {
+        int min = A[0], max = A[0];
+
+        for (int n : A) {
+            min = Math.min(min, n);
+            max = Math.max(max, n);
+        }
+
+        return (long) (max - min) * k;
+    }
+}
+
